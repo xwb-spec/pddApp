0cb3142d6787fea5e7562e452e43f1905bf76d98
Gogs <gogs@fake.local>
2022-09-07 20:09:56 +0800

    更新控件提示

--- yyui/show.go
+++ yyui/show.go
@@ -132,7 +132,7 @@ func (s *ShowInput) UploadedPicFileContainer() *fyne.Container {
 			uploadedPicFileEntry.SetText(reader.URI().Path()) //把读取到的路径显示到输入框中
 			s.UploadedImageConfig = uploadedPicFileEntry.Text
 		}, s.Win)
-		fd.SetFilter(storage.NewExtensionFileFilter([]string{".txt"})) //打开的文件格式类型
+		fd.SetFilter(storage.NewExtensionFileFilter([]string{".json"})) //打开的文件格式类型
 		fd.Show()
 	})
 	return container.New(layout.NewGridLayout(3), uploadedPicFileLabel, uploadedPicFileEntry, uploadedPicFileButton)
@@ -295,7 +295,11 @@ func (s *ShowInput) ButtonContainer() *fyne.Container {
 			s.ConsoleResult.SetText(resultConsole + "检测图片失败: [ERROR] 套图文件目录不存在" + s.PicKitDir)
 			return
 		}
-
+		isPathExist, err = common.IsPathExists(s.ShopExcel)
+		if err != nil {
+			s.ConsoleResult.SetText(resultConsole + "检测图片失败: [ERROR]: %s" + err.Error())
+			return
+		}
 	})
 	checkConfig := widget.NewButton("检测配置", func() {
 
@@ -329,4 +333,114 @@ func (s *ShowInput) MainShow(w fyne.Window) {
 		s.ButtonContainer(), s.ResultContainer(),
 	) //控制显示位置顺序
 	w.SetContent(box)
+}
+
+func (s *ShowInput) CheckInput() {
+	resultConsole := s.ConsoleResult.Text + "\n"
+	// 检测登录信息
+	if s.ShopId == "" {
+		s.ConsoleResult.SetText(resultConsole + "店铺id为空: [ERROR] 请输入店铺id")
+		return
+	}
+	if s.ShopName == "" {
+		s.ConsoleResult.SetText(resultConsole + "店铺名为空: [ERROR] 请输入店铺名")
+		return
+	}
+	// 检测店铺模板
+	if s.FreightTmp == "" {
+		s.ConsoleResult.SetText(resultConsole + "运费模板为空: [ERROR] 请输入运费模板")
+		return
+	}
+	// 检测公用图片
+	if s.PubFileDir == "" {
+		s.ConsoleResult.SetText(resultConsole + "公用文件目录为空: [ERROR] 请选择或输入公用文件目录" + s.PubFileDir)
+		return
+	}
+	isPathExist, err := common.IsPathExists(s.PubFileDir)
+	if err != nil {
+		s.ConsoleResult.SetText(resultConsole + "公用文件目录出错: [ERROR]: %s" + err.Error())
+		return
+	}
+	if !isPathExist {
+		s.ConsoleResult.SetText(resultConsole + "公用文件目录出错: [ERROR] 公用文件目录不存在" + s.PubFileDir)
+		return
+	}
+	// 检测套图
+	if s.PicKitDir == "" {
+		s.ConsoleResult.SetText(resultConsole + "套图文件目录为空: [ERROR] 请选择或输入套图文件目录" + s.PicKitDir)
+		return
+	}
+	isPathExist, err = common.IsPathExists(s.PicKitDir)
+	if err != nil {
+		s.ConsoleResult.SetText(resultConsole + "套图文件目录出错: [ERROR]: %s" + err.Error())
+		return
+	}
+	if !isPathExist {
+		s.ConsoleResult.SetText(resultConsole + "套图文件目录出错: [ERROR] 套图文件目录不存在" + s.PubFileDir)
+		return
+	}
+	// 检测配置文件路径
+	if s.UploadedImageConfig == "" {
+		s.ConsoleResult.SetText(resultConsole + "已上传图片文件配置为空: [ERROR] 请选择或输入已上传图片文件配置" + s.UploadedImageConfig)
+		return
+	}
+	isPathExist, err = common.IsPathExists(s.UploadedImageConfig)
+	if err != nil {
+		s.ConsoleResult.SetText(resultConsole + "已上传图片文件配置出错: [ERROR]: %s" + err.Error())
+		return
+	}
+	if !isPathExist {
+		s.ConsoleResult.SetText(resultConsole + "已上传图片文件配置出错: [ERROR] 已上传图片文件配置不存在" + s.UploadedImageConfig)
+		return
+	}
+	// 检测商品配置表
+	if s.ShopExcel == "" {
+		s.ConsoleResult.SetText(resultConsole + "商品配置表为空: [ERROR] 请选择或输入商品配置表" + s.ShopExcel)
+		return
+	}
+	isPathExist, err = common.IsPathExists(s.ShopExcel)
+	if err != nil {
+		s.ConsoleResult.SetText(resultConsole + "商品配置表出错: [ERROR]: %s" + err.Error())
+		return
+	}
+	if !isPathExist {
+		s.ConsoleResult.SetText(resultConsole + "商品配置表出错: [ERROR] 商品配置表不存在" + s.ShopExcel)
+		return
+	}
+	// 检测sku配置表
+	if s.SkuExcel == "" {
+		s.ConsoleResult.SetText(resultConsole + "sku配置表为空: [ERROR] 请选择或输入sku配置表" + s.SkuExcel)
+		return
+	}
+	isPathExist, err = common.IsPathExists(s.SkuExcel)
+	if err != nil {
+		s.ConsoleResult.SetText(resultConsole + "sku配置表出错: [ERROR]: %s" + err.Error())
+		return
+	}
+	if !isPathExist {
+		s.ConsoleResult.SetText(resultConsole + "sku配置表出错: [ERROR] sku配置表不存在" + s.SkuExcel)
+		return
+	}
+	// 检测型号配置表
+	if s.ModelExcel == "" {
+		s.ConsoleResult.SetText(resultConsole + "型号对照配置表为空: [ERROR] 请选择或输入型号对照配置表" + s.ModelExcel)
+		return
+	}
+	isPathExist, err = common.IsPathExists(s.ModelExcel)
+	if err != nil {
+		s.ConsoleResult.SetText(resultConsole + "型号对照配置表出错: [ERROR]: %s" + err.Error())
+		return
+	}
+	if !isPathExist {
+		s.ConsoleResult.SetText(resultConsole + "型号对照配置表出错: [ERROR] 型号对照配置表不存在" + s.ModelExcel)
+		return
+	}
+	// 检测表单
+	if s.ShopSheetName == "" {
+		s.ConsoleResult.SetText(resultConsole + "商品配置表表单为空: [ERROR] 请填写")
+	}
+	if s.ModelSheetName == "" {
+		s.ConsoleResult.SetText(resultConsole + "型号对照表表单为空: [ERROR] 请填写")
+	}
+
 }
