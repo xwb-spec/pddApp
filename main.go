313e1ec816673ab48217fdec9aeba3df95c6aa6b
qianmo020 <qianmo020@gmail.com>
2022-09-21 11:19:32 +0800

    修改字体

--- main.go
+++ main.go
@@ -1,43 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"github.com/flopp/go-findfont"
-	"os"
 	"pddApp/yyui"
-	"strings"
+	"pddApp/yyui/theme"
 )
 
 //设置字体
-func init() {
-	fontPaths := findfont.List()
-	for _, fontPath := range fontPaths {
-		//fmt.Println(fontPath)
-		//楷体:simkai.ttf
-		//黑体:simhei.ttf
-		//微软雅黑：msyh.ttc
-		if strings.Contains(fontPath, "/Users/machao/OptRepo/GolandProjects/awesomeProject/learning-golang/day41/simkai.ttf") {
-			err := os.Setenv("FYNE_FONT", fontPath)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			break
-		}
-	}
-}
+//func init() {
+//	fontPaths := findfont.List()
+//	for _, fontPath := range fontPaths {
+//		//fmt.Println(fontPath)
+//		//楷体:simkai.ttf
+//		//黑体:simhei.ttf
+//		//微软雅黑：msyh.ttc
+//		if strings.Contains(fontPath, "/Users/machao/OptRepo/GolandProjects/awesomeProject/learning-golang/day41/simkai.ttf") {
+//			err := os.Setenv("FYNE_FONT", fontPath)
+//			if err != nil {
+//				fmt.Println(err)
+//				return
+//			}
+//			break
+//		}
+//	}
+//}
 func runWin() {
-	err := os.Setenv("FYNE_FONT", "./simkai.ttf")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	//err := os.Setenv("FYNE_FONT", "./simkai.ttf")
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
 	//新建一个app
 	a := app.New()
 	//设置窗口栏，任务栏图标
 	//a.SetIcon(resourceIconPng)
+	a.Settings().SetTheme(&theme.MyTheme{})
 	//新建一个窗口
 	s := yyui.ShowInput{}
 	w := a.NewWindow("YY批量上链接V1.0   Xwb ALL Right Reserved QQ:543361609")
@@ -51,10 +49,10 @@ func runWin() {
 	//循环运行
 	w.ShowAndRun()
 
-	err = os.Unsetenv("FYNE_FONT")
-	if err != nil {
-		return
-	}
+	//err = os.Unsetenv("FYNE_FONT")
+	//if err != nil {
+	//	return
+	//}
 }
 func main() {
 	//ex := common.Comparisons{ExcelPath: "/Users/machao/Desktop/批量上商品app/批量上商品app/模板-型号图片对照表.xlsx", ExcelSheetName: "直边tpu"}
---
+++ yyui/theme/theme.go
@@ -0,0 +1,26 @@
+package theme
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+	"image/color"
+)
+
+type MyTheme struct{}
+
+var _ fyne.Theme = (*MyTheme)(nil)
+
+func (m MyTheme) Font(s fyne.TextStyle) fyne.Resource {
+	return resourceSimkaiTtf
+}
+func (*MyTheme) Color(n fyne.ThemeColorName, v fyne.ThemeVariant) color.Color {
+	return theme.DefaultTheme().Color(n, v)
+}
+
+func (*MyTheme) Icon(n fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(n)
+}
+
+func (*MyTheme) Size(n fyne.ThemeSizeName) float32 {
+	return theme.DefaultTheme().Size(n)
+}
