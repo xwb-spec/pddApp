e3600edc516f5896ae7e2ba497df51a4f45b8dc5
qianmo020 <qianmo020@gmail.com>
2022-09-27 14:22:00 +0800

    添加登录框

--- pinduoduo/server/main.go
+++ pinduoduo/server/main.go
@@ -7,15 +7,22 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/api/v1/callback/", func(c *gin.Context) {
-		if code, ok := c.GetQuery("code"); ok {
-			Save(ReturnCodeResponse{
-				Code:  code,
-				State: "xwb",
-			})
-			c.String(200, "callback code ok")
+		code, err := c.GetQuery("code")
+		if err != nil {
+			c.String(400, "callback code failed")
 			return
 		}
-		c.String(400, "callback code failed")
+		state, err := c.GetQuery("state")
+		if err != nil {
+			c.String(400, "callback code failed")
+			return
+		}
+		Save(ReturnCodeResponse{
+			Code:  code,
+			State: state,
+		})
+		c.String(200, "callback code ok")
+		return
 	})
 	r.POST("/api/v1/callback/", func(c *gin.Context) {
 		d := GetVal()
